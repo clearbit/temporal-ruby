bd2c4e42d7e6e60657a68210ef17347444201055
Guilherme Vieira <guilherme@clearbit.com>
2021-04-15 09:17:38 -0300

    Refactor serialization to converter. (#1)
    * Refactor serialization to converter.
    
    This follows the patterns of the Go and Java SDKs and allows
    interoperability between Ruby and the other SDKs by converting arguments
    to/from payloads in the same way.
    
    Serialisation of protobufs has been left as-is because it is not
    affected by interoperability issues.
    
    * Add some more payload specs.
    
    * Add mixed payloads test case.
    
    * Missed some callsites.
    
    * Make converters compatible.
    
    * PR feedback.
    
    Still need to bring a few specs back across.
    
    * Add legacy encoder.
    
    More specs.
    
    * Add legacy require.
    
    * Remove redundant parantheses.
    
    * Use a constant.
    
    * Add requires.
    
    * Add composite spec.
    
    * Remove attempt to migrate from legacy encoding.
    
    * Adjust ContinueAsNew serializer
    
    Authored-by: Rob Holland <rob@opus-grove.com>

---
+++ lib/temporal/client/converter/composite.rb
@@ -0,0 +1,57 @@
+module Temporal
+  module Client
+    module Converter
+      class Composite
+        class ConverterNotFound < RuntimeError; end
+        class MetadataNotSet < RuntimeError; end
+
+        def initialize(converters:)
+          @converters = converters
+          @converters_by_encoding = {}
+          converters.each do |converter|
+            @converters_by_encoding[converter.encoding] = converter
+          end
+        end
+
+        def from_payloads(payloads)
+          return nil if payloads.nil?
+          payloads.payloads.map(&method(:from_payload))
+        end
+
+        def from_payload(payload)
+          encoding = payload.metadata['encoding']
+          if encoding.nil?
+            raise MetadataNotSet
+          end
+
+          converter = converters_by_encoding[encoding]
+
+          if converter.nil?
+            raise ConverterNotFound
+          end
+
+          converter.from_payload(payload)
+        end
+
+        def to_payloads(*data)
+          Temporal::Api::Common::V1::Payloads.new(
+            payloads: data.map(&method(:to_payload))
+          )
+        end
+
+        def to_payload(data)
+          converters.each do |converter|
+            payload = converter.to_payload(data)
+            return payload unless payload.nil?
+          end
+
+          raise ConverterNotFound
+        end
+
+        private
+
+        attr_reader :converters, :converters_by_encoding
+      end
+    end
+  end
+end
